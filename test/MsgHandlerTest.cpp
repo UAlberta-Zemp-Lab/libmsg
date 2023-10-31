81b65ba6ea12e6ff7bbbcf12738cf60bf4da5013
Dahunsi-Darren-UAlberta <96056546+Dahunsi-Darren-UAlberta@users.noreply.github.com>
2023-10-30 18:03:17 -0600

    Parameterized MsgHandlerTest

--- test/MsgHandlerTest.cpp
+++ test/MsgHandlerTest.cpp
@@ -1,12 +1,7 @@
-#include "msg/MsgHandler.hpp"
+#include "MsgBaseTests.cpp"
 #include "util/DualPort.cpp"
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators_all.hpp>
-#include <future>
-#include <memory>
-#include <numeric>
-#include <stdint.h>
-#include <vector>
 
 TEST_CASE("Message Communciation", "[MsgHandler]") {
 	msg::DualPort<uint8_t> dualPort;
@@ -17,14 +12,5 @@ TEST_CASE("Message Communciation", "[MsgHandler]") {
 	    GENERATE(msg::Msg(), msg::Msg(msg::MsgType::DEBUG),
 	             msg::Msg(msg::MsgType::UNDEF, 5,
 	                      std::vector<uint8_t>{ 1, 2, 3, 4, 5 }));
-	msg::Msg receivedMsg;
-
-	std::future<void> write = std::async(
-	    msg::MsgHandler::writeMsg, std::ref(message), std::ref(portA));
-	std::future<msg::Msg> read =
-	    std::async(msg::MsgHandler::readMsg, std::ref(portB));
-	write.wait();
-	read.wait();
-	receivedMsg = read.get();
-	REQUIRE(receivedMsg == (msg::Msg)message);
+	msg::checkMsgHandling(message, portA, portB);
 }
