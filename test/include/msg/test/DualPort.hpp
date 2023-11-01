3ce018bd36314b1f3f77edfd91c2a750c888582a
Dahunsi-Darren-UAlberta <96056546+Dahunsi-Darren-UAlberta@users.noreply.github.com>
2023-11-01 15:11:23 -0600

    Fixed concurrency issue in Dual Port
    moved mutex guards directly around queue access and added condition variable for notifing of write events

--- test/include/msg/test/DualPort.hpp
+++ test/include/msg/test/DualPort.hpp
@@ -1,5 +1,7 @@
 #ifndef _MSG_DUALPORT_H
 #define _MSG_DUALPORT_H
+#include <memory>
+#include <vector>
 
 namespace msg {
 template <typename T> class DualPort {
@@ -7,8 +9,11 @@ public:
 	class Port;
 
 	DualPort()
-	    : queueAtoB(), queueBtoA(), A(queueBtoA, queueAtoB),
-	      B(queueAtoB, queueBtoA) {}
+	    : queueAtoB(), queueBtoA(),
+	      A(queueBtoA, queueAtoB, mtxBtoA, mtxAtoB, conditionBtoA,
+	        conditionAtoB),
+	      B(queueAtoB, queueBtoA, mtxAtoB, mtxBtoA, conditionAtoB,
+	        conditionBtoA) {}
 
 	Port &GetPortA() { return this->A; }
 	Port &GetPortB() { return this->B; }
@@ -16,15 +21,17 @@ public:
 private:
 	std::queue<T> queueAtoB;
 	std::queue<T> queueBtoA;
+	std::mutex mtxAtoB;
+	std::mutex mtxBtoA;
+	std::condition_variable conditionAtoB;
+	std::condition_variable conditionBtoA;
 	Port A;
 	Port B;
 };
 
 template <typename T> class DualPort<T>::Port : public msg::Serial<T> {
 public:
-	Port(std::queue<T> &readQueue, std::queue<T> &writeQueue)
-	    : readQueue(readQueue), writeQueue(writeQueue) {}
-
+	friend class DualPort;
 	std::vector<T> read(unsigned int len);
 	void write(const std::vector<T> data);
 
@@ -38,22 +45,28 @@ public:
 	                 std::chrono::microseconds writeTimeout);
 
 private:
+	Port(std::queue<T> &readQueue, std::queue<T> &writeQueue,
+	     std::mutex &readMtx, std::mutex &writeMtx,
+	     std::condition_variable &readCondition,
+	     std::condition_variable &writeCondition)
+	    : readQueue(readQueue), writeQueue(writeQueue), readMtx(readMtx),
+	      writeMtx(writeMtx), readCondition(readCondition),
+	      writeCondition(writeCondition) {}
 	std::queue<T> &readQueue;
 	std::queue<T> &writeQueue;
+	std::mutex &readMtx;
+	std::mutex &writeMtx;
+	std::condition_variable &readCondition;
+	std::condition_variable &writeCondition;
 	std::chrono::microseconds readTimeout = std::chrono::milliseconds(1000);
 	std::chrono::microseconds writeTimeout =
 	    std::chrono::milliseconds(1000);
-	std::unique_ptr<std::mutex> readMtx = std::make_unique<std::mutex>();
-	std::unique_ptr<std::mutex> writeMtx = std::make_unique<std::mutex>();
 };
 
 template <typename T>
 std::vector<T>
 DualPort<T>::Port::read(unsigned int len) {
 	namespace chrono = std::chrono;
-	// Multi-thread read saftey
-	auto lock = std::lock_guard(*(this->readMtx));
-
 	// Read Output
 	std::vector<T> output;
 
@@ -64,6 +77,12 @@ DualPort<T>::Port::read(unsigned int len) {
 	};
 
 	do {
+		std::unique_lock<std::mutex> readLock(this->readMtx);
+
+		readCondition.wait_for(
+		    readLock, std::chrono::microseconds(100),
+		    [this] { return this->readQueue.size() > 0; });
+
 		// If Data available
 		while (this->readQueue.size() > 0 && output.size() < len) {
 			// Read Data & delete from buffer
@@ -74,7 +93,6 @@ DualPort<T>::Port::read(unsigned int len) {
 		if (output.size() == len) {
 			break;
 		}
-		std::this_thread::sleep_for(std::chrono::microseconds(100));
 	} while (elaspedTime() < this->readTimeout);
 
 	// Check if sufficient data read
@@ -89,11 +107,14 @@ template <typename T>
 void
 DualPort<T>::Port::write(const std::vector<T> data) {
 	// Multi-thread write saftey
-	auto lock = std::lock_guard(*(this->writeMtx));
+	{
+		auto lock = std::lock_guard(this->writeMtx);
 
-	for (auto d : data) {
-		this->writeQueue.push(d);
+		for (auto d : data) {
+			this->writeQueue.push(d);
+		}
 	}
+	this->writeCondition.notify_one();
 }
 
 template <typename T>
@@ -107,7 +128,7 @@ template <typename T>
 void
 DualPort<T>::Port::flushRead() {
 	// Multi-thread read saftey
-	auto lock = std::lock_guard(*(this->readMtx));
+	auto lock = std::lock_guard(this->readMtx);
 
 	while (this->readQueue.size() > 0) {
 		this->readQueue.pop(); // Potential data race with writer
@@ -118,7 +139,7 @@ template <typename T>
 void
 DualPort<T>::Port::flushWrite() {
 	// Multi-thread write saftey
-	auto lock = std::lock_guard(*(this->writeMtx));
+	auto lock = std::lock_guard(this->writeMtx);
 
 	while (this->writeQueue.size() > 0) {
 		this->writeQueue.pop(); // Potential data race with reader
