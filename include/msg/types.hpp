4cc5f96c4ddb050c77ee344be90e9db1746239f8
Dahunsi-Darren-UAlberta <96056546+Dahunsi-Darren-UAlberta@users.noreply.github.com>
2023-11-01 15:13:03 -0600

    Moved typedef from Msg.hpp to types.hpp

--- include/msg/types.hpp
+++ include/msg/types.hpp
@@ -1,5 +1,7 @@
 #include <chrono>
 
 namespace msg {
+typedef std::vector<uint8_t> byteArray;
+typedef byteArray::const_iterator byteConstIter;
 typedef std::chrono::time_point<std::chrono::high_resolution_clock> timeStamp;
-}
+} // namespace msg
