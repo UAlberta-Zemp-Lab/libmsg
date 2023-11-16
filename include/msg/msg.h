1fa4b304681b624a66b37686523a37286db1ef08
Randy Palamar <palamar@ualberta.ca>
2023-11-16 15:47:27 -0700

    drop MSG_REJECT
    There isn't an internal use case for this type.

--- include/msg/msg.h
+++ include/msg/msg.h
@@ -8,9 +8,8 @@
 /* Reserved Message Types */
 #define MSG_ERR      0xFF00
 #define MSG_ACK      0xFF01
-#define MSG_REJECT   0xFF02
-#define MSG_CONTINUE 0xFF03
-#define MSG_RETRY    0xFF04
+#define MSG_CONTINUE 0xFF02
+#define MSG_RETRY    0xFF03
 #define MSG_DEBUG    0xFFFF
 
 /* Type Definitions */
