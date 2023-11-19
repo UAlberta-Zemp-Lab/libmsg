5ebb979ce9f9a354ee542e2b46cc34e66e51f067
Randy Palamar <palamar@ualberta.ca>
2023-11-19 14:52:27 -0700

    split msg types into msgtypes.h

--- include/msg/msg.h
+++ include/msg/msg.h
@@ -2,16 +2,9 @@
 #ifndef _MSG_H
 #define _MSG_H
 
+#include <msg/msgtypes.h>
 #include <stdbool.h>
 #include <stdint.h>
-
-/* Reserved Message Types */
-#define MSG_ERR      0xFF00
-#define MSG_ACK      0xFF01
-#define MSG_CONTINUE 0xFF02
-#define MSG_RETRY    0xFF03
-#define MSG_STOP     0xFF04
-#define MSG_DEBUG    0xFFFF
 
 /* Type Definitions */
 typedef struct {
---
+++ include/msg/msgtypes.h
@@ -0,0 +1,12 @@
+#ifndef _MSGTYPES_H
+#define _MSGTYPES_H
+
+/* Reserved Message Types */
+#define MSG_ERR      0xFF00
+#define MSG_ACK      0xFF01
+#define MSG_CONTINUE 0xFF02
+#define MSG_RETRY    0xFF03
+#define MSG_STOP     0xFF04
+#define MSG_DEBUG    0xFFFF
+
+#endif /* _MSGTYPES_H */
