183057e604cc4f8cea3935be4067f7755a4ed23f
Randy Palamar <palamar@ualberta.ca>
2023-11-20 22:12:25 -0700

    add unistd test for write failure

--- test/unistd-test.c
+++ test/unistd-test.c
@@ -7,8 +7,8 @@
 
 #include "utils.h"
 
-void
-msg_pipe(Msg *txm, Msg *rxm)
+static void
+msg_pipe(Msg *txm, Msg *rxm, bool expected_fail)
 {
 	int parent[2], child[2];
 
@@ -25,7 +25,7 @@ msg_pipe(Msg *txm, Msg *rxm)
 		MsgUnistdDev d = { .rfd = parent[0], .wfd = child[1] };
 		MsgStream s;
 		msg_unistd_init(&s, &d);
-		if (!msg_write(&s, txm))
+		if (!msg_write(&s, txm) && !expected_fail)
 			die("child failed to write msg");
 		exit(0);
 	} else { /* parent process */
@@ -34,7 +34,7 @@ msg_pipe(Msg *txm, Msg *rxm)
 		msg_unistd_init(&s, &d);
 		if (!s.available(s.dev))
 			die("parent has no data available");
-		if (!msg_read(&s, rxm))
+		if (!msg_read(&s, rxm) && !expected_fail)
 			die("parent failed to read msg");
 	}
 
@@ -44,17 +44,36 @@ msg_pipe(Msg *txm, Msg *rxm)
 	close(child[1]);
 }
 
+static void
+test_tx_rx(void)
+{
+	uint8_t data[4] = { 0xde, 0xad, 0xbe, 0xef };
+	Msg m1 = { .type = 0x1234, .length = 4, .data = data }, m2 = { 0 };
+
+	msg_pipe(&m1, &m2, false);
+	assert_msg_equal(&m1, &m2);
+}
+
+static void
+test_msg_stop(void)
+{
+	Msg m1 = { .type = MSG_CONTINUE, .length = 0 }, m2 = { 0 };
+
+	/* parent will read MSG_CONTINUE from child and send MSG_STOP
+	 * child will fail after receiving MSG_STOP from parent
+	 */
+	msg_pipe(&m1, &m2, true);
+	assert_msg_equal(&m1, &m2);
+}
+
 int
 main(void)
 {
 	/* hack around meson's broken buffering */
 	setvbuf(stdout, NULL, _IONBF, 0);
 
-	uint8_t data[4] = { 0xde, 0xad, 0xbe, 0xef };
-	Msg m1 = { .type = 0x1234, .length = 4, .data = data }, m2;
-
-	msg_pipe(&m1, &m2);
-	assert_msg_equal(&m1, &m2);
+	test_tx_rx();
+	test_msg_stop();
 
 	return 0;
 }
