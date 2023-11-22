8c4625d5b27ba4b0a46de18f66cc5a4ee592d5ff
zemplab <124933999+zemplab@users.noreply.github.com>
2023-11-22 10:40:10 -0700

    Created seperate cpp Msg implementation
    Added cpp header, source, and test file for cpp implementation of Msg class.  Additionally added catch2 as a dependency for cpp testing, modified Meson build file to separately build a cpp library.  Renamed c library to differentiate it from cpp library.

---
+++ include/msg/Msg.hpp
@@ -0,0 +1,60 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef _MSG_HPP
+#define _MSG_HPP
+
+#include <map>
+#include <memory>
+#include <msg/MsgType.hpp>
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+/* Type Definitions */
+namespace msg {
+class Msg {
+	// Assumes little-endianess
+	using byteArray = std::vector<uint8_t>;
+	using byteConstIter = byteArray::const_iterator;
+
+public:
+	static constexpr size_t sizeOfType = sizeof(MsgType::type);
+	static constexpr size_t sizeOfLength = sizeof(uint16_t);
+	static constexpr size_t sizeOfHeader = sizeOfType + sizeOfLength;
+
+	Msg();
+	Msg(uint16_t type);
+	Msg(uint16_t type, uint16_t length);
+	Msg(uint16_t type, uint16_t length, const byteArray &data);
+	Msg(const byteArray &bytes);
+	Msg(byteConstIter, byteConstIter);
+
+	uint16_t type() const;
+	uint16_t length() const;
+	const byteArray &data() const;
+
+	void setType(uint16_t type);
+	void setData(const byteArray &, uint16_t length);
+	void setData(byteConstIter, byteConstIter, uint16_t length);
+
+	// Array's & Iterator's layout and size must match Msg specifications
+	void readMsg(const byteArray &);
+	void readMsg(byteConstIter, byteConstIter);
+	void readHeader(const byteArray &);
+	void readHeader(byteConstIter, byteConstIter);
+	void readData(const byteArray &);
+	void readData(byteConstIter, byteConstIter);
+
+	byteArray writeMsg() const;
+	byteArray writeHeader() const;
+	byteArray writeData() const;
+
+	operator byteArray() const;
+	bool operator==(const Msg &other) const;
+
+private:
+	uint16_t _type;
+	byteArray _data;
+};
+} // namespace msg
+
+#endif
