59973bf44f8fb93ffc22ef21b73eae03f2ed6dc1
Dahunsi-Darren-UAlberta <96056546+Dahunsi-Darren-UAlberta@users.noreply.github.com>
2024-02-15 13:10:12 -0700

    Added Stream & Serial interfaces
    Additionally added DualPort class as a concrete implementation of Serial interface

---
+++ include/msg/Stream.hpp
@@ -0,0 +1,44 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef _MSG_STREAM_HPP
+#define _MSG_STREAM_HPP
+
+#include <chrono>
+#include <vector>
+
+namespace msg {
+struct StreamOptions {
+	enum WritePriority { Master, Slave };
+	WritePriority WritePriority = WritePriority::Master;
+};
+
+template <class T> class Stream {
+public:
+	StreamOptions options;
+	virtual void write(const std::vector<T> data) = 0;
+	virtual std::vector<T> read(unsigned int len) = 0;
+	Stream<T> &operator<<(const std::vector<T> &data) {
+		this->write(data);
+		return *this;
+	}
+	Stream<T> &operator>>(std::vector<T> &data) {
+		std::vector<T> readData = this->read(data.size());
+		data.assign(readData.begin(), readData.end());
+		return *this;
+	}
+	virtual ~Stream() = default;
+};
+
+template <class T> class Serial : public Stream<T> {
+public:
+	virtual void flush() = 0;
+	virtual void flushRead() = 0;
+	virtual void flushWrite() = 0;
+	virtual unsigned int numberOfBytesAvailable() const = 0;
+	virtual bool isDataAvailable() const = 0;
+	virtual bool isConnected() const = 0;
+	virtual void setTimeouts(std::chrono::microseconds readTimeout,
+	                         std::chrono::microseconds writeTimeout) = 0;
+};
+} // namespace msg
+
+#endif
