3906a07d5d0766b29daa45ce3dcdf24eaf981643
Darren Dahunsi <96056546+Dahunsi-Darren-UAlberta@users.noreply.github.com>
2024-02-15 20:56:25 -0700

    Created seperate cpp Msg implementation
    Added cpp header, source, and test file for cpp implementation of Msg class.  Additionally added catch2 as a dependency for cpp testing, modified Meson build file to separately build a cpp library.  Renamed c library to differentiate it from cpp library.

---
+++ src/msg/Msg.cpp
@@ -0,0 +1,137 @@
+/* See LICENSE file for copyright and license details. */
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <msg/Msg.hpp>
+#include <stdbool.h>
+#include <stdexcept>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+
+using byteArray = std::vector<uint8_t>;
+using byteConstIter = byteArray::const_iterator;
+
+namespace msg {
+Msg::Msg() : Msg::Msg(MsgType::ERR) {}
+Msg::Msg(uint16_t type) : Msg(type, 0) {}
+Msg::Msg(uint16_t type, uint16_t length)
+    : _type(type), _data(byteArray(length, 0)) {}
+Msg::Msg(uint16_t type, uint16_t length, const byteArray &data) : Msg(type, 0) {
+	setData(data, length);
+}
+Msg::Msg(const byteArray &bytes) : Msg(bytes.begin(), bytes.end()) {}
+Msg::Msg(byteConstIter begin, byteConstIter end) : Msg() {
+	readMsg(begin, end);
+}
+
+uint16_t
+Msg::type() const {
+	return _type;
+}
+uint16_t
+Msg::length() const {
+	return static_cast<uint16_t>(_data.size());
+}
+const byteArray &
+Msg::data() const {
+	return _data;
+}
+
+void
+Msg::setType(uint16_t type) {
+	_type = type;
+}
+void
+Msg::setData(const byteArray &data, uint16_t length) {
+	setData(data.begin(), data.end(), length);
+}
+void
+Msg::setData(byteConstIter begin, byteConstIter end, uint16_t length) {
+	if (std::distance(begin, end) < length) {
+		throw std::runtime_error("Data not large enough");
+	}
+
+	_data.assign(begin, begin + length);
+}
+
+void
+Msg::readMsg(const byteArray &bytes) {
+	readMsg(bytes.begin(), bytes.end());
+}
+
+void
+Msg::readMsg(byteConstIter begin, byteConstIter end) {
+
+	this->readHeader(begin, end);
+	if (length() > 0 && std::distance(begin + sizeOfHeader, end) != 0) {
+		readData(begin + sizeOfHeader, end);
+	}
+}
+
+void
+Msg::readHeader(const byteArray &bytes) {
+	readHeader(bytes.begin(), bytes.end());
+}
+
+void
+Msg::readHeader(byteConstIter begin, byteConstIter end) {
+	if (std::distance(begin, end) > 0
+	    && static_cast<size_t>(std::distance(begin, end)) < sizeOfHeader) {
+		throw std::runtime_error("Range too short to construct header");
+	}
+
+	uint16_t type = (*(begin + 1) << 8) | (*begin);
+	std::advance(begin, 2);
+	uint16_t _length = (*(begin + 1) << 8) | (*begin);
+	std::advance(begin, 2);
+
+	_data.resize(_length);
+	_type = type;
+}
+
+void
+Msg::readData(const byteArray &bytes) {
+	readData(bytes.begin(), bytes.end());
+}
+
+void
+Msg::readData(byteConstIter begin, byteConstIter end) {
+	if (std::distance(begin, end) != length()) {
+		throw std::runtime_error("Data not correct size");
+	}
+
+	_data.assign(begin, end);
+}
+
+byteArray
+Msg::writeHeader() const {
+	return byteArray{
+		static_cast<uint8_t>(_type & 0xff),
+		static_cast<uint8_t>(_type >> 8),
+		static_cast<uint8_t>(length() & 0xff),
+		static_cast<uint8_t>(length() >> 8),
+	};
+}
+
+byteArray
+Msg::writeData() const {
+	return data();
+}
+
+byteArray
+Msg::writeMsg() const {
+	byteArray msg = writeHeader();
+	msg.reserve(sizeOfHeader + length());
+	msg.insert(msg.end(), _data.begin(), _data.end());
+	return msg;
+}
+
+Msg::operator byteArray() const { return writeMsg(); }
+} // namespace msg
+
+bool
+msg::Msg::operator==(const Msg &other) const {
+	return (this == &other)
+	       || ((_type == other._type) && (_data == other._data));
+}
---
+++ test/MsgTest.cpp
@@ -0,0 +1,219 @@
+
+#include <assert.h>
+#include <msg/Msg.hpp>
+#include <numeric>
+#include <stdint.h>
+#include <vector>
+
+void testConstructors();
+void testSet();
+void testRead();
+void testWrite();
+void testOperators();
+
+int
+main() {
+	testConstructors();
+	testSet();
+	testRead();
+	testWrite();
+	testOperators();
+}
+
+void
+testConstructors() {
+	uint16_t type = 0xABCD;
+	uint16_t length = 25;
+	std::vector<uint8_t> data = std::vector<uint8_t>();
+	std::vector<uint8_t> bytes = std::vector<uint8_t>();
+	msg::Msg msg;
+
+	data.resize(length);
+	std::iota(data.begin(), data.end(), static_cast<uint8_t>(1));
+	bytes.insert(bytes.end(), {
+				      static_cast<uint8_t>(type & 0xff),
+				      static_cast<uint8_t>(type >> 8),
+				      static_cast<uint8_t>(length & 0xff),
+				      static_cast<uint8_t>(length >> 8),
+				  });
+	bytes.insert(bytes.end(), data.begin(), data.end());
+
+	assert(msg.type() == msg::MsgType::ERR);
+	assert(msg.length() == 0);
+	assert(msg.data().empty());
+
+	msg = msg::Msg();
+	assert(msg.type() == msg::MsgType::ERR);
+	assert(msg.length() == 0);
+	assert(msg.data().empty());
+
+	msg = msg::Msg(type);
+	assert(msg.type() == type);
+	assert(msg.length() == 0);
+	assert(msg.data().empty());
+
+	msg = msg::Msg(type, length);
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == std::vector<uint8_t>(length, 0));
+
+	msg = msg::Msg(type, length, data);
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+
+	msg = msg::Msg(bytes);
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+
+	msg = msg::Msg(bytes.begin(), bytes.end());
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+}
+
+void
+testSet() {
+	uint16_t type = 0xABCD;
+	uint16_t length = 25;
+	std::vector<uint8_t> data = std::vector<uint8_t>();
+	msg::Msg msg;
+
+	data.resize(length);
+	std::iota(data.begin(), data.end(), static_cast<uint8_t>(1));
+
+	msg = msg::Msg();
+	msg.setType(type);
+	assert(msg.type() == type);
+	assert(msg.length() == 0);
+	assert(msg.data().empty());
+
+	msg = msg::Msg();
+	msg.setData(data, length);
+	assert(msg.type() == msg::MsgType::ERR);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+
+	msg = msg::Msg();
+	msg.setData(data.begin(), data.end(), length);
+	assert(msg.type() == msg::MsgType::ERR);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+}
+void
+testRead() {
+	uint16_t type = 0xABCD;
+	uint16_t length = 25;
+	std::vector<uint8_t> header = std::vector<uint8_t>();
+	std::vector<uint8_t> data = std::vector<uint8_t>();
+	std::vector<uint8_t> bytes = std::vector<uint8_t>();
+	msg::Msg msg;
+
+	header = std::vector<uint8_t>{
+		static_cast<uint8_t>(type & 0xff),
+		static_cast<uint8_t>(type >> 8),
+		static_cast<uint8_t>(length & 0xff),
+		static_cast<uint8_t>(length >> 8),
+	};
+	data.resize(length);
+	std::iota(data.begin(), data.end(), static_cast<uint8_t>(1));
+	bytes.insert(bytes.end(), header.begin(), header.end());
+	bytes.insert(bytes.end(), data.begin(), data.end());
+
+	msg = msg::Msg();
+	msg.readMsg(bytes);
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+
+	msg = msg::Msg();
+	msg.readMsg(bytes.begin(), bytes.end());
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+
+	msg = msg::Msg();
+	msg.readHeader(header);
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == std::vector<uint8_t>(length, 0));
+
+	msg = msg::Msg();
+	msg.readHeader(header.begin(), header.end());
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == std::vector<uint8_t>(length, 0));
+
+	msg = msg::Msg(header);
+	msg.readData(data);
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+
+	msg = msg::Msg(type, length);
+	msg.readData(data);
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+
+	msg = msg::Msg(type, length);
+	msg.readData(data.begin(), data.end());
+	assert(msg.type() == type);
+	assert(msg.length() == length);
+	assert(msg.data() == data);
+}
+void
+testWrite() {
+	uint16_t type = 0xABCD;
+	uint16_t length = 25;
+	std::vector<uint8_t> header = std::vector<uint8_t>();
+	std::vector<uint8_t> data = std::vector<uint8_t>();
+	std::vector<uint8_t> bytes = std::vector<uint8_t>();
+	msg::Msg msg;
+
+	header = std::vector<uint8_t>{
+		static_cast<uint8_t>(type & 0xff),
+		static_cast<uint8_t>(type >> 8),
+		static_cast<uint8_t>(length & 0xff),
+		static_cast<uint8_t>(length >> 8),
+	};
+	data.resize(length);
+	std::iota(data.begin(), data.end(), static_cast<uint8_t>(1));
+	bytes.insert(bytes.end(), header.begin(), header.end());
+	bytes.insert(bytes.end(), data.begin(), data.end());
+
+	msg = msg::Msg(type, length, data);
+
+	assert(msg.writeMsg() == bytes);
+	assert(msg.writeHeader() == header);
+	assert(msg.writeData() == data);
+}
+void
+testOperators() {
+	uint16_t type = 0xABCD;
+	uint16_t length = 25;
+	std::vector<uint8_t> header = std::vector<uint8_t>();
+	std::vector<uint8_t> data = std::vector<uint8_t>();
+	std::vector<uint8_t> bytes = std::vector<uint8_t>();
+	msg::Msg msg1;
+	msg::Msg msg2;
+
+	header = std::vector<uint8_t>{
+		static_cast<uint8_t>(type & 0xff),
+		static_cast<uint8_t>(type >> 8),
+		static_cast<uint8_t>(length & 0xff),
+		static_cast<uint8_t>(length >> 8),
+	};
+	data.resize(length);
+	std::iota(data.begin(), data.end(), static_cast<uint8_t>(1));
+	bytes.insert(bytes.end(), header.begin(), header.end());
+	bytes.insert(bytes.end(), data.begin(), data.end());
+
+	msg1 = msg::Msg(type, length, data);
+	assert(msg1 == bytes);
+
+	msg1 = msg::Msg(bytes);
+	msg2 = msg::Msg(type, length, data);
+	assert(msg1 == msg2);
+}
